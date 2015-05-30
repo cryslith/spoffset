1afbe593fdf5ecb91f43cf33d42eb69113a584d7
Lily Chung <lkdc@mit.edu>
2015-05-29 23:28:03 -0400

    print bytes directly; allow hex value with -x

--- spoffset.c
+++ spoffset.c
@@ -1,19 +1,49 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <getopt.h>
 
 unsigned long get_sp(void) {
   __asm__("movl %esp,%eax");
 }
 
+void usage() {
+  fprintf(stderr, "usage: spoffset [-x] offset\n");
+}
+
 int main(int argc, char **argv) {
-  if (argc != 2) {
-    fprintf(stderr, "usage: spoffset offset\n");
+  int printhex = 0;
+  char oc;
+
+  while ((oc = getopt(argc, argv, "x")) != -1) {
+    switch(oc) {
+    case 'x':
+      printhex = 1;
+      break;
+    default:
+      usage();
+      return 1;
+    }
+  }
+
+  if (argc != optind + 1) {
+    usage();
     return 1;
   }
 
-  long offset = atol(argv[1]);
+  long offset = atol(argv[optind]);
 
   unsigned long sp = get_sp();
-  printf("0x%lx\n", sp - offset);
+  unsigned long addr = sp - offset;
+
+  if (printhex) {
+    printf("0x%lx\n", addr);
+  }
+  else {
+    char bytes[4] = {(char) addr,
+                     (char) (addr >> 8),
+                     (char) (addr >> 16),
+                     (char) (addr >> 24)};
+    fwrite(bytes, 1, 4, stdout);
+  }
   return 0;
 }
